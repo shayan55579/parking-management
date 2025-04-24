ee00ba7ef82fc21edbbf658fce0605d4f7b2cb14
shayan55579 <shayanebrahimi555@yahoo.com>
2025-04-24 10:31:42 +0430

    tailwind and proje

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-indigo-700 text-white text-3xl font-bold">
+      ✅ Tailwind is working!
+    </div>
+  );
+}
+
+export default App
